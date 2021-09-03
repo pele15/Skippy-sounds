6fd3d7440aa48858755b356b70e85447dd6786f6
Praneeth Eddu <pele15@carbonorigins.com>
2021-09-03 15:20:35 -0500

    AIO credentials

--- voiceOfSkippy.py
+++ voiceOfSkippy.py
@@ -1,19 +1,5 @@
+ADAFRUIT_IO_USERNAME = "pele15"
+ADAFRUIT_IO_KEY = "aio_GfHf16A3RCSsqTFx1Xn5nUuenF7a"
+
 from playsound import playsound
-
 playsound('sounds/h.wav')
-
-# from pydub import AudioSegment
-# from pydub.playback import play
-  
-# # for playing wav file
-# song = AudioSegment.from_wav("hiSkippy.wav")
-# #song = AudioSegment.from_wav()
-# print('playing sound using  pydub')
-# play(song)
-
-# import simpleaudio as sa
-
-# filename = 'hiSkippy.wav'
-# wave_obj = sa.WaveObject.from_wave_file(filename)
-# play_obj = wave_obj.play()
-# play_obj.wait_done()  # Wait until sound has finished playing
