a039427eb19516b85777222b061b58acfac583e4
Praneeth Eddu <praneeth@carbonorigins.com>
2021-09-03 16:16:12 -0400

    removed playsound

--- voiceOfSkippy.py
+++ voiceOfSkippy.py
@@ -1,19 +1,8 @@
-from playsound import playsound
-
-playsound('sounds/h.wav')
-
-# from pydub import AudioSegment
-# from pydub.playback import play
+from pydub import AudioSegment
+from pydub.playback import play
   
 # # for playing wav file
-# song = AudioSegment.from_wav("hiSkippy.wav")
-# #song = AudioSegment.from_wav()
-# print('playing sound using  pydub')
-# play(song)
+song = AudioSegment.from_wav("sounds/h.wav")
+print('playing sound using  pydub')
+play(song)
 
-# import simpleaudio as sa
-
-# filename = 'hiSkippy.wav'
-# wave_obj = sa.WaveObject.from_wave_file(filename)
-# play_obj = wave_obj.play()
-# play_obj.wait_done()  # Wait until sound has finished playing
