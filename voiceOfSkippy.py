82b39862ac83b5e5a4f5fa9ed5dc383782bbb713
Praneeth Eddu <pele15@carbonorigins.com>
2021-09-03 15:59:26 -0500

    hello feed update

--- voiceOfSkippy.py
+++ voiceOfSkippy.py
@@ -1,26 +1,26 @@
  # import Adafruit IO REST client.
 from Adafruit_IO import Client, Feed, RequestError
-from pydub import AudioSegment
-from pydub.playback import play
+# from pydub import AudioSegment
+# from pydub.playback import play
   
 
 ADAFRUIT_IO_USERNAME = "pele15"
-ADAFRUIT_IO_KEY = "aio_GfHf16A3RCSsqTFx1Xn5nUuenF7a"
+ADAFRUIT_IO_KEY = "aio_DRdm94V29fs1KhUmUB0pG4gj2l06"
 
 aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
 
-try:
-    hellofeed = aio.feeds('hello')
-# # for playing wav file
-except RequestError: # create feeds
-    feed = Feed(name="hello")
-    hellofeed = aio.create_feed(feed)
-
-song = AudioSegment.from_wav("sounds/h.wav")
-print('playing sound using  pydub')
+# try:
+#     hellofeed = aio.feeds('hello')
+# except RequestError: # create feeds
+#     feed = Feed(name="hello")
+#     hellofeed = aio.create_feed(feed)
+hellofeed = aio.feeds('hello-key')
+# song = AudioSegment.from_wav("sounds/h.wav")
+# print('playing sound using  pydub')
 
 while True:
-    hello = aio.recieve(hello.key)
-    if (int(hello.value)):
-        play(song)
+    hello = aio.receive(hellofeed.key)
+    if (int(hello.value) == 1):
+        print('hi')
+        #play(song)
 
