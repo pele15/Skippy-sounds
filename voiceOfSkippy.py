5c44d6e28853533614884e288984d23829730f10
Praneeth Eddu <praneeth@carbonorigins.com>
2021-09-03 22:44:52 -0400

    pickle works

--- voiceOfSkippy.py
+++ voiceOfSkippy.py
@@ -2,10 +2,11 @@
 from Adafruit_IO import Client, Feed, RequestError
 from pydub import AudioSegment
 from pydub.playback import play
-  
+import time  
 
 ADAFRUIT_IO_USERNAME = "pele15"
-ADAFRUIT_IO_KEY = "aio_NGId95PrxExxSWBSAzVQbj4HyJUI"
+
+ADAFRUIT_IO_KEY = "aio_lMFJ91KR0BjAS2hohaJkr5x923yN"
 
 aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
 
@@ -17,17 +18,21 @@ aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
 
 
 hellofeed = aio.feeds('hello-key')
-picklefeed = aio.feeds('pickle-feed')
+picklefeed = aio.feeds('pickle')
 skippyHello = AudioSegment.from_wav("sounds/h.wav")
 pickleSound = AudioSegment.from_wav("sounds/p.wav")
 # print('playing sound using  pydub')
 
 while True:
+    start = time.time()
     hello = aio.receive(hellofeed.key)
-    pickle = aio.receive(pickle.key)
-    if (int(hello.value) == 1):
-        #print('hi')
+    pickle = aio.receive(picklefeed.key)
+   # print(hello.value)
+    if (int(hello.value) == 1 and int(hello.value) != 45):
+        print('hi')
         play(skippyHello)
-    elif (int(pickle.value) == 1):
+    elif (int(pickle.value) == 1 and int(pickle.value) != 45):
         print('pickle')
         play(pickleSound)
+    end = time.time()
+    print(end - start)
