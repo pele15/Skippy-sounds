a6d929d6946dd203b53aa0aacd5c2f2b822f6cb5
Praneeth Eddu <pele15@carbonorigins.com>
2021-09-03 21:18:47 -0500

    change key

--- voiceOfSkippy.py
+++ voiceOfSkippy.py
@@ -5,7 +5,7 @@ from pydub.playback import play
   
 
 ADAFRUIT_IO_USERNAME = "pele15"
-ADAFRUIT_IO_KEY = "aio_OpZB40phr1U0eoJZ1dV2Oly0jnm9"
+ADAFRUIT_IO_KEY = "aio_tudh78bpUrDCJg8w9gEsaMpUv4YQ"
 
 aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
 
