dbe0d737a8f33246617897adbe22a669d922c0ab
Praneeth Eddu <pele15@carbonorigins.com>
2021-09-14 09:53:50 -0500

    hi

--- mqttSkippy.py
+++ mqttSkippy.py
@@ -9,19 +9,18 @@ from pydub.playback import play
 
 # Import Adafruit IO MQTT client.
 from Adafruit_IO import MQTTClient
+import random
 
 # Set to your Adafruit IO key.
 # Remember, your key is a secret,
 # so make sure not to publish it when you publish this code!
-ADAFRUIT_IO_KEY = 'aio_ufnG66YR6w11qTidAZBri2oGpYET'
+ADAFRUIT_IO_KEY = 'aio_sssN36O1kpV4LH7qc4YpYj1m2RcY'
 
 # Set to your Adafruit IO username.
 # (go to https://accounts.adafruit.com to find your username)
 ADAFRUIT_IO_USERNAME = 'carbonorigins'
 
 # Set to the ID of the feed to subscribe to for updates.
-# FEED_IDs = ['hello-key', 'bye-feed']
-# FEED_SOUNDS = ["h.wav", "p.wav"]
 FEED_IDs_SOUNDS_DICT = {
     'hello-key': "hello.wav",
     'bye-feed': "bye.wav",
@@ -30,15 +29,8 @@ FEED_IDs_SOUNDS_DICT = {
     'friend': "friend.wav",
     'skippy': "skippy.wav",
     'jokes': "jokes.wav",
-    'cross-road': "cross-road.wav",
-    'cross-road-resp': "cross-road-resp.wav",
-    'sweden': "sweden.wav",
-    'sweden-resp': "sweden-resp.wav",
-    'guac': "guac.wav",
-    'guac-resp': "guac-resp.wav",
     'drink': "drink.wav",
     'food-btn': "food.wav",
-    'breakfast': "breakfast.wav",
     'great': "great.wav",
     'ad': "ad.wav",
     'notsure': "notsure.wav",
@@ -46,12 +38,33 @@ FEED_IDs_SOUNDS_DICT = {
     'food-list': "food-list.wav",
     'noprob':"noprob.wav",
     'deliver':"deliver.wav",
-    'vaccum':"vaccum.wav",
-    'vaccum-resp':"vaccum-resp.wav",
     'way':"way.wav",
-    'roll':"roll.wav"
+    'roll':"roll.wav", 
+    'joke-question': "", # limit,
+    'punchline': "",
+    'caribou': "caribou.wav",
+    'potbelly': "potbelly.wav",
+    # 'pickup':"rene.wav",
+    # 'open':"open.wav",
+    # 'close':"close.wav",
+    # 'thanks':"thanks.wav"
 }
+
+JOKES_DICT = {
+    'guac':"guac-resp",
+    'sweden':"sweden-resp",
+    'vaccum':"vaccum-resp",
+    'cross-road': "cross-road-resp",
+    'texas':"texas-resp"
+}
+
+global joke_key, ind
+ind = 0
+joke_key = ""
 FEED_IDs = FEED_IDs_SOUNDS_DICT.keys()
+JOKES_IDs = list(JOKES_DICT.keys())
+JOKES_IDs_INDS = random.sample(JOKES_IDs, len(JOKES_IDs))
+
 
 # Define callback functions which will be called when certain events happen.
 def connected(client):
@@ -80,17 +93,31 @@ def message(client, feed_id, payload):
     # The feed_id parameter identifies the feed, and the payload parameter has
     # the new value.
     print('Feed {0} received new value: {1}'.format(feed_id, payload))
+    global joke_key
+    global ind
     if payload == "1":
         try:
-            audio = AudioSegment.from_wav("sounds/" + str(FEED_IDs_SOUNDS_DICT[feed_id]))
+            if (feed_id == "joke-question"):
+                joke_key = JOKES_IDs_INDS[ind]
+                ind = (ind + 1) % len(JOKES_IDs)
+                audio = AudioSegment.from_wav("sounds/" + str(joke_key) + ".wav")
+
+            elif (feed_id == "punchline"):
+                print("elif")
+                print("sounds/" + str(JOKES_DICT[joke_key]) + ".wav")
+                audio = AudioSegment.from_wav("sounds/" + str(JOKES_DICT[joke_key]) + ".wav")
+            else:
+                print("else")
+                audio = AudioSegment.from_wav("sounds/" + str(FEED_IDs_SOUNDS_DICT[feed_id]))
             play(audio)
         except:
             print('Couldn\'t find the audio file. Please add one')
             pass
 
+
+print("Total feed to be subscribed: ", len(FEED_IDs))
 # Create an MQTT client instance.
 client = MQTTClient(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
-
 # Setup the callback functions defined above.
 client.on_connect    = connected
 client.on_disconnect = disconnected
