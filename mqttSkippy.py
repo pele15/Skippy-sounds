bd8b3a599706345c465396d28230e8cdf5739720
Praneeth Eddu <pele15@carbonorigins.com>
2021-09-18 15:33:58 -0500

    startup

--- mqttSkippy.py
+++ mqttSkippy.py
@@ -34,8 +34,8 @@ FEED_IDs_SOUNDS_DICT = {
     'roll':"roll.wav", 
     'joke-question': "",
     'punchline': "",
-    #'caribou': "caribou.wav",
-    #'potbelly': "potbelly.wav",
+    'caribou': "caribou.wav",
+    'potbelly': "potbelly.wav",
     'scan': "scan.wav",
     'hungry':"hungry.wav",
     # 'pickup':"rene.wav",
@@ -45,7 +45,7 @@ FEED_IDs_SOUNDS_DICT = {
     'yes':"sean.wav",
     'thankyou':"thankyou.wav",
     'ricos': "ricos.wav",
-    'ziscuit': "ziscuit.wav",
+    'zis': "ziscuit.wav",
     'seven': "seven.wav",
 }
 
@@ -58,23 +58,54 @@ JOKES_DICT = {
     'metal': "metal-resp",
     'insecure':"insecure-resp",
     'rusty':"rusty-resp",
-    'rust':"rust-resp",
     'battery':"battery-resp",
 }
 
+STARTUP_EVENT_DICT = {
+    'hello': "hello",
+    'ready': "ready",
+    'beer': "beer",
+    'takebeer': "takebeer",
+    'scan': "scan",
+    'selfie': "selfie",
+    'selfiebeer': "selfiebeer",
+    'takebeer': "takebeer",
+    'skipster': "skipster"
+}
+
+STARTUP_FEED_COUNT= {
+    'beer': 1,
+    'scan': 1,
+    'selfie': 2,
+    'takebeer': 2,
+}
+
+
+GENERAL_DICT = {
+    'skippy': "skippy.wav",
+    'jokes': "jokes.wav",
+    'joke-question': "",
+    'punchline': "",
+    'bye-feed': "bye.wav",
+    'thankyou':"thankyou.wav",
+    'bye-feed': "bye.wav",
+}
+
+
+STARTUP_FEED_IDs = ['startup.' + k for k in STARTUP_EVENT_DICT.keys()]
 global joke_key, ind
 ind = 0
 joke_key = ""
-FEED_IDs = FEED_IDs_SOUNDS_DICT.keys()
+
+
+#FEED_IDs = FEED_IDs_SOUNDS_DICT.keys()
+FEED_IDs = STARTUP_FEED_IDs
+FEED_IDs = FEED_IDs + list(GENERAL_DICT.keys())
 JOKES_IDs = list(JOKES_DICT.keys())
 JOKES_IDs_INDS = random.sample(JOKES_IDs, len(JOKES_IDs))
 
 # Define callback functions which will be called when certain events happen.
 def connected(client):
-    # Connected function will be called when the client is connected to Adafruit IO.
-    # This is a good place to subscribe to feed changes.  The client parameter
-    # passed to this function is the Adafruit IO MQTT client so you can make
-    # calls against it easily.
     
     print('Connected to Adafruit IO!  Listening for {0} changes...'.format(FEED_IDs))
     # Subscribe to changes on a feed named DemoFeed.
@@ -92,9 +123,6 @@ def disconnected(client):
     sys.exit(1)
 
 def message(client, feed_id, payload):
-    # Message function will be called when a subscribed feed has a new value.
-    # The feed_id parameter identifies the feed, and the payload parameter has
-    # the new value.
     print('Feed {0} received new value: {1}'.format(feed_id, payload))
     global joke_key
     global ind
@@ -104,9 +132,15 @@ def message(client, feed_id, payload):
                 joke_key = JOKES_IDs_INDS[ind]
                 ind = (ind + 1) % len(JOKES_IDs)
                 audio = AudioSegment.from_wav("sounds/" + str(joke_key) + ".wav")
-
             elif (feed_id == "punchline"):
                 audio = AudioSegment.from_wav("sounds/" + str(JOKES_DICT[joke_key]) + ".wav")
+            elif feed_id.startswith('startup.'):
+                try:
+                    maxrange = str(STARTUP_FEED_COUNT[feed_id.replace("startup.", "")])
+                    feed_count = random.randint(1, int(maxrange))
+                    audio = AudioSegment.from_wav("sounds/hackYourCity/" + str(STARTUP_EVENT_DICT[feed_id.replace("startup.", "")]) + "-" + str(feed_count) + ".wav")
+                except:
+                    audio = AudioSegment.from_wav("sounds/hackYourCity/" + str(STARTUP_EVENT_DICT[feed_id.replace("startup.", "")]) + ".wav")
             else:
                 audio = AudioSegment.from_wav("sounds/" + str(FEED_IDs_SOUNDS_DICT[feed_id]))
             play(audio)
@@ -134,7 +168,4 @@ client.on_subscribe  = subscribe
 # Connect to the Adafruit IO server.
 client.connect()
 
-# Start a message loop that blocks forever waiting for MQTT messages to be
-# received.  Note there are other options for running the event loop like doing
-# so in a background thread--see the mqtt_client.py example to learn more.
 client.loop_blocking()
